bd5444d3026510029dc8eb5cf90dc8cde1847ac3
Diksha126 <62324089+Diksha126@users.noreply.github.com>
2020-03-18 11:46:00 +0530

    Add files via upload

---
+++ project1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<sys/wait.h>
+#include<unistd.h>
+int main()
+{
+ int pid,n;
+ printf("WELCOME TO THE PROGRAM\n");
+ do{
+    printf("ENTER A POSITIVE NUMBER TO RUN!!\n");
+    scanf("%d",&n);
+    }while(n<=0);
+ pid=fork();
+ if(pid==0)
+  {
+   printf("child process is running\n");
+   printf("%d\n",n);
+   while(n!=1)
+   {
+   if(n%2==0)
+   n=(n/2);
+   printf("%d\n",n);
+   }
+   printf("child process is done\n");
+   }
+ else
+   {
+    wait(NULL);
+    printf("parent process is done\n");
+   }
+}
+
