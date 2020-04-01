8c1d24036b7f38e6fcd341f7da6f54a408978233
Diksha126 <62324089+Diksha126@users.noreply.github.com>
2020-04-01 20:10:01 +0530

    Update project1.c

--- project1.c
+++ project1.c
@@ -4,15 +4,14 @@
 int main()
 {
  int pid,n;
- printf("WELCOME TO THE PROGRAM....\n");
  do{
-    printf("ENTER A VALID NUMBER TO RUN!!\n");
+    printf("ENTER!!\n");
     scanf("%d",&n);
     }while(n<=0);
  pid=fork();
  if(pid==0)
   {
-   printf("child process is running\n");
+   printf("child process \n");
    printf("%d\n",n);
    while(n!=1)
    {
@@ -20,13 +19,10 @@ int main()
    n=(n/2);
    printf("%d\n",n);
    }
-   printf("child process is done\n");
    }
  else
-   {
-    printf("parent process is waiting\n");
+ {
     wait(NULL);
-    printf("parent process is done\n");
-   }
+ }
 }
 
